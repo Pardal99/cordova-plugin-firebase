aab50e07304d422644cc26999c2c2bf70aa2d2b0
Pablo Pardal Garces <pablo.pardalgarces@soprasteria.com>
2018-12-05 15:27:40 +0100

    more changes for google services

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -17,7 +17,31 @@ var name = utilities.getValue(config, 'name');
 var IOS_DIR = 'platforms/ios';
 var ANDROID_DIR = 'platforms/android';
 
-var PLATFORM = {};
+var PLATFORM = {
+  IOS: {
+    dest: [
+      IOS_DIR + '/' + name + '/Resources/GoogleService-Info.plist',
+      IOS_DIR + '/' + name + '/Resources/Resources/GoogleService-Info.plist'
+    ],
+    src: [
+      'GoogleService-Info.plist',
+      IOS_DIR + '/www/GoogleService-Info.plist',
+      'www/GoogleService-Info.plist'
+    ]
+  },
+  ANDROID: {
+    dest: [
+      ANDROID_DIR + '/google-services.json',
+      ANDROID_DIR + '/app/google-services.json'
+    ],
+    src: [
+      'google-services.json',
+      ANDROID_DIR + '/assets/www/google-services.json',
+      'www/google-services.json',
+      ANDROID_DIR + '/app/src/main/google-services.json'
+    ],
+  }
+};
 
 module.exports = function (context) {
   //get platform from the context supplied by cordova
@@ -25,10 +49,10 @@ module.exports = function (context) {
   // Copy key files to their platform specific folders
   if (platforms.indexOf('ios') !== -1 && utilities.directoryExists(IOS_DIR)) {
     console.log('Preparing Firebase on iOS');
-    utilities.copyKey(PLATFORM.IOS);
+    // utilities.copyKey(PLATFORM.IOS);
   }
   if (platforms.indexOf('android') !== -1 && utilities.directoryExists(ANDROID_DIR)) {
     console.log('Preparing Firebase on Android');
-    utilities.copyKey(PLATFORM.ANDROID);
+    // utilities.copyKey(PLATFORM.ANDROID);
   }
 };
